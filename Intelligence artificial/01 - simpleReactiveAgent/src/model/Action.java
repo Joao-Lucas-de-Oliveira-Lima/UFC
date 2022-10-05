5d6774d6cb2d30d38e7fdf1460da3ce4e486dfd0
Joao-Lucas-de-Oliveira-Lima <63080135+Joao-Lucas-de-Oliveira-Lima@users.noreply.github.com>
2022-10-05 15:17:57 -0300

    study material

--- Intelligence artificial/01 - simpleReactiveAgent/src/model/Action.java
+++ Intelligence artificial/01 - simpleReactiveAgent/src/model/Action.java
@@ -4,9 +4,6 @@ public class Action {
     private String name;
 
     public Action(){}
-    public String getName(){
-        return this.name;
-    }
     public void setName(String name){
         this.name = name;
     }
