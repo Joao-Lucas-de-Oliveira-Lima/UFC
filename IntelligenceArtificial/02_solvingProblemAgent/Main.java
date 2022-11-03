1c06dc046614ea01a79d0bc636932aac6bf0e041
Joao-Lucas-de-Oliveira-Lima <63080135+Joao-Lucas-de-Oliveira-Lima@users.noreply.github.com>
2022-11-03 11:29:37 -0300

    Test

--- IntelligenceArtificial/02_solvingProblemAgent/Main.java
+++ IntelligenceArtificial/02_solvingProblemAgent/Main.java
@@ -41,11 +41,11 @@ public class Main {
         //System.out.print("\nEnter the target state: ");
         //String destination = reader.nextLine();
         //Search in width result
-        Solution solution = searchInWidth.performSearch(romaniaMap, args[0], "Bucharest");
-        System.out.println("\nSearch in width: "+solution.toString()+"\n");
+        //Solution solution = searchInWidth.performSearch(romaniaMap, args[0], "Bucharest");
+        //System.out.println("\nSearch in width: "+solution.toString()+"\n");
         //Uniform cost search result
-        //solution = uniformCostSearch.performSearch(romaniaMap, "Arad", destination);
-        //System.out.println("Uniform cost search: "+solution.toString()+"\n");
+        Solution solution = uniformCostSearch.performSearch(romaniaMap, args[0], "Bucharest");
+        System.out.println("Uniform cost search: "+solution.toString()+"\n");
         //Depth search result
         //solution = depthSearch.performSearch(romaniaMap, "Arad", destination);
         System.out.println("Depth search: "+solution.toString());
